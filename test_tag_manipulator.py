a36b7cb456a16139d337e5e7acb957aaba084d61
rugerharms <hlharms19@gmail.com>
2021-07-29 16:28:23 +0100

    cut3 - test with two inputs returns splitted string

--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -14,15 +14,31 @@ def test_split_empty_string_result_empty_array():
     # assert
     assert result == expResult
 
-    def test_split_comma_empty_string_result_empty_array():
-        # arrange
-        stringToSplit = ","
-        expResult = []
-        result = None
-        cut = TagManipulator()
 
-        # act
-        result = cut.parse_string(stringToSplit)
+def test_split_comma_empty_string_result_empty_array():
+    # arrange
+    stringToSplit = ","
+    expResult = []
+    result = None
+    cut = TagManipulator()
 
-        # assert
-        assert result == expResult
+    # act
+    result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
+
+
+def test_split_one_string_result_array_of_one():
+    # arrange
+    stringToSplit = "java"
+    regex = ","
+    expResult = ["java"]
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit, regex)
+
+    # assert
+    assert result == expResult
